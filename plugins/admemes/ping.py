36a505ffe107f034165e19c0b094213816427926
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-07-23 12:19:02 +0530

    Ping Module

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -8,10 +8,10 @@ from info import COMMAND_HAND_LER
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # -- Constants -- #
-ALIVE = "ചത്തിട്ടില്ല മുത്തേ ഇവിടെ തന്നെ ഉണ്ട്.. നിനക്ക് ഇപ്പൊ എന്നോട് ഒരു സ്നേഹവും ഇല്ല. കൊള്ളാം.. നീ പാഴെ പോലെയേ അല്ല മാറിപോയി..😔 ഇടക്ക് എങ്കിലും ചുമ്മാ ഒന്ന് /start ചെയ്തു നോക്ക്..🙂" 
-REPO = "<b>𝙳𝙴𝙿𝙻𝙾𝚈 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 ›› https://youtu.be/kB9TkCs8cX0</b>"
-CHANNEL = "<b>𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻</b> ›› https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA\n\n<b>𝚄𝙿𝙳𝙰𝚃𝙴𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/OpusTechz</b>\n\n<b>𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/MWUpdatez</b>"
-AJAX = "<b>𝙱𝙾𝚃 ›› https://t.me/Devil0Bot_Bot</b>"
+ALIVE = "Hey There I'm Alive Thanks For Checking On Me 🥰"
+REPO = "<b>MY REPO ›› https://github.com/TechnoMindz/Extra_Feature_Bot</b>"
+CHANNEL = "<b>𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻</b> ›› https://www.youtube.com/channel/UCP07nv5a8HjRd_h_PDQLG6g\n\n<b>𝚄𝙿𝙳𝙰𝚃𝙴𝚂 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/tmmainchannel</b>\n\n<b>𝙲𝙷𝙰𝙽𝙽𝙴𝙻 ›› https://t.me/tmmainchannel</b>"
+RAVI = "<b>𝙱𝙾𝚃 ›› https://t.me/TmELsaRoBot</b>"
 # -- Constants End -- #
 
 
@@ -44,8 +44,8 @@ async def channel(_, message):
     await message.reply_text(CHANNEL)
 
 
-@Client.on_message(filters.command("ajax", COMMAND_HAND_LER) & f_onw_fliter)
-async def ajax(_, message):
-    await message.reply_text(AJAX)
+@Client.on_message(filters.command("ravi", COMMAND_HAND_LER) & f_onw_fliter)
+async def ravi(_, message):
+    await message.reply_text(RAVI)
 
 
