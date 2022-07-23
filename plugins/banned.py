159607be9a95e2800f939f41794e9855242274eb
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-07-23 12:28:23 +0530

    Banned Module
    Evamaria

--- plugins/banned.py
+++ plugins/banned.py
@@ -20,7 +20,7 @@ disabled_group=filters.create(disabled_chat)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'Sorry Dude, You are Banned in the Techno Mindz Fedration So you cant able To Use Me \nBan Reason: {ban["ban_reason"]}')
+    await message.reply(f'Sorry Dude, You are Banned in the Techno Mindz Fedration So you cant able To Use Me \nBan Reason: {ban["ban_reason"]}\nContact @TechnoMindzChat To Know More')
 
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
