00d5c5eea2adcdb0118f7088a0b6ccdd4c05ff04
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-07-23 10:47:59 +0530

    Small Update For Users

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -24,7 +24,7 @@ async def add_filter(grp_id, text, reply_text, btn, file, alert):
     try:
         mycol.update_one({'text': str(text)},  {"$set": data}, upsert=True)
     except:
-        logger.exception('Some error occured!', exc_info=True)
+        logger.exception('Some error occured 😵‍💫', exc_info=True)
              
      
 async def find_filter(group_id, name):
@@ -68,25 +68,25 @@ async def delete_filter(message, text, group_id):
     if query == 1:
         mycol.delete_one(myquery)
         await message.reply_text(
-            f"'`{text}`'  deleted. I'll not respond to that filter anymore.",
+            f"'`{text}`'  Done ✅ Deleted I'll Not Respond To That Filter Anymore 😉",
             quote=True,
             parse_mode="md"
         )
     else:
-        await message.reply_text("Couldn't find that filter!", quote=True)
+        await message.reply_text("Couldn't find that filter🥴", quote=True)
 
 
 async def del_all(message, group_id, title):
     if str(group_id) not in mydb.list_collection_names():
-        await message.edit_text(f"Nothing to remove in {title}!")
+        await message.edit_text(f"Nothing to remove in {title}! 🥴")
         return
 
     mycol = mydb[str(group_id)]
     try:
         mycol.drop()
-        await message.edit_text(f"All filters from {title} has been removed")
+        await message.edit_text(f"All filters from {title} has been removed 😮‍💨")
     except:
-        await message.edit_text("Couldn't remove all filters from group!")
+        await message.edit_text("Couldn't remove all filters from group! 😵‍💫")
         return
 
 
