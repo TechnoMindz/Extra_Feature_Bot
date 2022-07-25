e2378fe488faf8981084e000dc34279bcb044fd4
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-07-25 16:52:37 +0530

    Bug Fixes

--- plugins/admemes/tts.py
+++ plugins/admemes/tts.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message
 
 def convert(text):
     audio = BytesIO()
-    i = Translator().translate(text, dest="Techno Mindz")
+    i = Translator().translate(text, dest="en")
     lang = i.src
     tts = gTTS(text, lang=lang)
     audio.name = lang + ".mp3"
