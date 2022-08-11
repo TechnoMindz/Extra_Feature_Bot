791df60f96eb65671b0ce43baea8bf9c689a81c4
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-08-11 19:10:36 +0530

    Test Update 1

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-@Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
+@Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
