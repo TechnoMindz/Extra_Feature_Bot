618d5faa0707474a02326ac8d601421343c70c00
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2022-08-11 19:27:39 +0530

    Updates Maybe Later

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name=SESSION,
+            session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
